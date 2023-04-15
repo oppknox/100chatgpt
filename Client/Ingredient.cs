ba81e0349ee6e9469bbc372e557138490e35f8d7
oppknox <seanknox@outlook.com>
2023-04-14 22:11:38 -0700

    Initial receipe form

---
+++ Client/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿public class Ingredient
+{
+    public string? Name { get; set; }
+    public double Amount { get; set; }
+    public string? Unit { get; set; }
+
+    public static readonly List<string> CommonUnits = new List<string>
+    {
+        "tsp", "tbsp", "fl oz", "cup", "pint", "quart", "gallon", "ml", "cl", "dl", "l", "mg", "g", "kg", "oz", "lb"
+    };
+}
---
+++ Client/Recipe.cs
@@ -0,0 +1,21 @@
+﻿public class Recipe
+{
+    public string? Title { get; set; }
+    public string? Instructions { get; set; }
+    public int Rating { get; set; }
+    public int Servings { get; set; }
+    public int PrepTime { get; set; }
+    public int CookTime { get; set; }
+    public string? Difficulty { get; set; }
+    public string? Nutrition { get; set; }
+    public string? Notes { get; set; }
+
+    public int TotalTime => PrepTime + CookTime;
+
+    public List<Ingredient> Ingredients { get; set; }
+
+    public Recipe()
+    {
+        Ingredients = new List<Ingredient>();
+    }
+}
